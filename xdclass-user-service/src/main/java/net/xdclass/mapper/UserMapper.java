bb2974267a4af936067372b6f4ed7cbc02158c40
ertong <2841119399@qq.com>
2022-05-29 22:55:28 +0800

    用户微服务逆向工程代码新增

---
+++ xdclass-user-service/src/main/java/net/xdclass/mapper/UserMapper.java
@@ -0,0 +1,9 @@
+package net.xdclass.mapper;
+
+import net.xdclass.model.UserDO;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+
+public interface UserMapper extends BaseMapper<UserDO> {
+
+}
