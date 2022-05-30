71122890a3a25a92c9741842973aff229cc12cb3
ertong <2841119399@qq.com>
2022-05-30 14:42:43 +0800

    自定义异常

--- xdclass-common/src/main/java/net/xdclass/exception/CustomExceptionHandler.java
+++ xdclass-common/src/main/java/net/xdclass/exception/CustomExceptionHandler.java
@@ -19,11 +19,11 @@ public class CustomExceptionHandler {
         if (e instanceof BizException) {
 
             BizException bizException = (BizException) e;
-            log.error("[业务异常 {}]", e);
+            log.error("[业务异常] { }", e);
 
             return JsonData.buildCodeAndMsg(bizException.getCode(), bizException.getMsg());
         } else {
-            log.error("[非业务异常 {}]", e);
+            log.error("[非业务异常] { }" , e);
 
             return JsonData.buildError("全局异常，未知错误");
         }
